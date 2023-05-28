78796df6ac486015b6a5cd0105b6d0d2884c026a
4yda4ok <134729295+4yda4ok@users.noreply.github.com>
2023-05-29 00:03:16 +0300

    Fixed names for script path

--- main.py
+++ main.py
@@ -6,42 +6,42 @@ import subprocess
 
 print("\n1/8 Downloading data from server")
 
-script_path = "firstScript.py"
+script_path = "1.py"
 subprocess.call(["python", script_path])
 
 print("\n2/8 Creating tables")
 
-script_path = "secondScript.py"
+script_path = "2.py"
 subprocess.call(["python", script_path])
 
 print("\n3/8 Convert files to .csv")
 
-script_path = "thirdScript.py"
+script_path = "3.py"
 subprocess.call(["python", script_path])
 
 print("\n4/8 Convert map files to .csv")
 
-script_path = "fourthScript.py"
+script_path = "4.py"
 subprocess.call(["python", script_path])
 
 print("\n5/8 Convert dataset to .csv")
 
-script_path = "fifthScript.py"
+script_path = "5.py"
 subprocess.call(["python", script_path])
 
 print("\n6/8 Creating tables from .csv")
 
-script_path = "sixScript.py"
+script_path = "6.py"
 subprocess.call(["python", script_path])
 
 print("\n7/8 Export .csv files to postgresql server")
 
-script_path = "sevenScript.py"
+script_path = "7.py"
 subprocess.call(["python", script_path])
 
 print("\n8/8 Making wordls map from table")
 
-script_path = "eightScript.py"
+script_path = "8.py"
 subprocess.call(["python", script_path])
 
 print("\nSuccessfully")
